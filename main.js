5e2edc402638b2540a2b6d69061dac2fb766f9e3
Afikur Rahman <this.afikur@gmail.com>
2019-11-08 04:37:43 +0600

    init commit

---
+++ main.js
@@ -0,0 +1,36 @@
+var cbpAnimatedHeader = (function() {
+  var docElem = document.documentElement,
+    header = document.querySelector(".header"),
+    didScroll = false,
+    changeHeaderOn = 150;
+
+  function init() {
+    scrollPage();
+    window.addEventListener(
+      "scroll",
+      function(event) {
+        if (!didScroll) {
+          didScroll = true;
+          setTimeout(scrollPage, 250);
+        }
+      },
+      false
+    );
+  }
+
+  function scrollPage() {
+    var sy = scrollY();
+    if (sy >= changeHeaderOn) {
+      header.classList.add("header-shrink");
+    } else {
+      header.classList.remove("header-shrink");
+    }
+    didScroll = false;
+  }
+
+  function scrollY() {
+    return window.pageYOffset || docElem.scrollTop;
+  }
+
+  init();
+})();
